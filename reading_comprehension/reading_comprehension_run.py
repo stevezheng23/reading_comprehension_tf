1b6592ea676fceb68be2af62e864c00f3725863c
Mingzhi Zheng <mizheng@microsoft.com>
2018-05-07 22:11:00 +0800

    add subword-level representation layer support

--- reading_comprehension/reading_comprehension_run.py
+++ reading_comprehension/reading_comprehension_run.py
@@ -94,16 +94,17 @@ def test(logger,
     
     train_sess.run(train_model.data_pipeline.initializer)
     
-    (input_question_word, input_question_char, input_question_word_feat,
-         input_question_char_feat) = train_sess.run([train_model.data_pipeline.input_question_word,
-             train_model.data_pipeline.input_question_char, train_model.model.input_question_word_feat,
+    (input_question_word, input_question_subword, input_question_char, input_question_word_feat,
+         input_question_subword_feat, input_question_char_feat) = train_sess.run([train_model.data_pipeline.input_question_word,
+             train_model.data_pipeline.input_question_subword, train_model.data_pipeline.input_question_char,
+             train_model.model.input_question_word_feat, train_model.model.input_question_subword_feat,
              train_model.model.input_question_char_feat])
-    #input_char_embedding_shape = train_sess.run([train_model.model.input_char_embedding_shape])
     print(input_question_word)
+    print(input_question_subword)
     print(input_question_char)
     print(input_question_word_feat)
+    print(input_question_subword_feat)
     print(input_question_char_feat)
-    #print(input_char_embedding_shape)
 
 def main(args):
     hyperparams = load_hyperparams(args.config)
