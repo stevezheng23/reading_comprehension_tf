3b1a3b49fc8fb1665b68226f212d247a453961bb
Mingzhi Zheng <mizheng@microsoft.com>
2018-05-11 00:33:02 +0800

    add rnn as understanding layer for bidaf

--- reading_comprehension/util/layer_util.py
+++ reading_comprehension/util/layer_util.py
@@ -5,9 +5,10 @@ from layer.embedding import *
 from layer.convolution import *
 from layer.pooling import *
 from layer.highway import *
+from layer.recurrent import *
 
 __all__ = ["create_embedding_layer", "create_convolution_layer", "create_pooling_layer",
-           "create_highway_layer"]
+           "create_highway_layer", "create_recurrent_layer"]
 
 def create_embedding_layer(vocab_size,
                            embed_dim,
@@ -33,10 +34,10 @@ def create_convolution_layer(conv_type,
     scope = "conv/{0}".format(conv_type)
     if conv_type == "1d":
         conv_layer = Conv1D(num_filter=num_filter, window_size=window_size, stride_size=stride_size,
-            padding_type=padding_type, activation=activation, trainable=trainable, scope=conv_type)
+            padding_type=padding_type, activation=activation, trainable=trainable, scope=scope)
     elif conv_type == "2d":
         conv_layer = Conv2D(num_channel=num_channel, num_filter=num_filter, window_size=window_size, stride_size=stride_size,
-            padding_type=padding_type, activation=activation, trainable=trainable, scope=conv_type)
+            padding_type=padding_type, activation=activation, trainable=trainable, scope=scope)
     else:
         raise ValueError("unsupported convolution type {0}".format(conv_type))
     
@@ -76,7 +77,8 @@ def create_single_highway_layer(highway_type,
     """create single highway layer"""
     scope = "highway/{0}".format(highway_type)
     if highway_type == "full_connected":
-        highway_layer = Highway(unit_dim=unit_dim, activation=activation, trainable=trainable)
+        highway_layer = Highway(unit_dim=unit_dim,
+            activation=activation, trainable=trainable, scope=scope)
     else:
         raise ValueError("unsupported highway type {0}".format(highway_type))
     
@@ -90,9 +92,35 @@ def create_stacked_highway_layer(highway_type,
     """create stacked highway layer"""
     scope = "stacked_highway/{0}".format(highway_type)
     if highway_type == "full_connected":
-        highway_layer = StackedHighway(num_layer=num_layer,
-            unit_dim=unit_dim, activation=activation, trainable=trainable)
+        highway_layer = StackedHighway(num_layer=num_layer, unit_dim=unit_dim,
+            activation=activation, trainable=trainable, scope=scope)
     else:
         raise ValueError("unsupported highway type {0}".format(highway_type))
     
     return highway_layer
+
+def create_recurrent_layer(recurrent_type,
+                           num_layer,
+                           unit_dim,
+                           cell_type,
+                           activation,
+                           drop_out,
+                           forget_bias,
+                           residual_connect,
+                           num_gpus,
+                           default_gpu_id,
+                           trainable):
+    """create recurrent layer"""
+    scope = "recurrent/{0}".format(recurrent_type)
+    if recurrent_type == "uni_directional":
+        recurrent_layer = RNN(num_layer=num_layer, unit_dim=unit_dim, cell_type=cell_type,
+            activation=activation, drop_out=drop_out, forget_bias=forget_bias, residual_connect=residual_connect,
+            num_gpus=num_gpus, default_gpu_id=default_gpu_id, trainable=trainable, scope=scope)
+    elif recurrent_type == "bi_directional":
+        recurrent_layer = BiRNN(num_layer=num_layer, unit_dim=unit_dim, cell_type=cell_type,
+            activation=activation, drop_out=drop_out, forget_bias=forget_bias, residual_connect=residual_connect,
+            num_gpus=num_gpus, default_gpu_id=default_gpu_id, trainable=trainable, scope=scope)
+    else:
+        raise ValueError("unsupported recurrent type {0}".format(recurrent_type))
+    
+    return recurrent_layer
