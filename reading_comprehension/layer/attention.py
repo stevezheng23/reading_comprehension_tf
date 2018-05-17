fix masking issue & fix variable scope issue

reading_comprehension/layer/attention.py
@@ -128,14 +128,14 @@ class Attention(object):
                  input_trg_mask):
         """call attention layer"""
         with tf.variable_scope(self.scope, reuse=tf.AUTO_REUSE):
-            input_src_data = input_src_data * tf.expand_dims(input_src_mask, axis=-1)
-            input_trg_data = input_trg_data * tf.expand_dims(input_trg_mask, axis=-1)
+            input_src_data = input_src_data * input_src_mask
+            input_trg_data = input_trg_data * input_trg_mask
             input_attention_score = _generate_attention_score(input_src_data,
                 input_trg_data, self.attention_matrix, self.score_type)
             input_attention_weight = tf.nn.softmax(input_attention_score, dim=1)
             output_attention = tf.matmul(input_attention_weight, input_trg_data)
-            
-            return output_attention
+        
+        return output_attention
     
     def get_attention_matrix():
         return self.attention_matrix
@@ -160,8 +160,8 @@ class MaxAttention(Attention):
                  input_trg_mask):
         """call maximum attention layer"""
         with tf.variable_scope(self.scope, reuse=tf.AUTO_REUSE):
-            input_src_data = input_src_data * tf.expand_dims(input_src_mask, axis=-1)
-            input_trg_data = input_trg_data * tf.expand_dims(input_trg_mask, axis=-1)
+            input_src_data = input_src_data * input_src_mask
+            input_trg_data = input_trg_data * input_trg_mask
             input_attention_score = _generate_attention_score(input_src_data,
                 input_trg_data, self.attention_matrix, self.score_type)
             input_attention_score = tf.reduce_max(input_attention_score, axis=-1)
@@ -170,5 +170,5 @@ class MaxAttention(Attention):
             output_attention = tf.matmul(input_attention_weight, input_src_data)
             src_max_length = tf.shape(input_src_data)[1]
             output_attention = tf.tile(output_attention, multiples=[1, src_max_length, 1])
-            
-            return output_attention
+        
+        return output_attention

reading_comprehension/layer/convolution.py
@@ -51,8 +51,8 @@ class Conv1D(Conv):
         """call 1d convolution layer"""
         with tf.variable_scope(self.scope, reuse=tf.AUTO_REUSE):
             output_conv = self.conv_layer(input_data)
-            
-            return output_conv
+        
+        return output_conv
 
 class Conv2D(Conv):
     """2d convolution layer"""
@@ -86,5 +86,5 @@ class Conv2D(Conv):
             dim2_length = input_conv_shape[1]
             output_conv = tf.reshape(input_conv,
                 shape=[batch_size, dim1_length, dim2_length, self.num_filter])
-            
-            return output_conv
+        
+        return output_conv

reading_comprehension/layer/embedding.py
@@ -29,8 +29,8 @@ class Embedding(object):
         """call embedding layer"""
         with tf.variable_scope(self.scope, reuse=tf.AUTO_REUSE):
             output_embedding = tf.nn.embedding_lookup(self.embedding, input_data)
-            
-            return output_embedding, self.embedding_placeholder
+        
+        return output_embedding, self.embedding_placeholder
 
 class PretrainedEmbedding(object):
     """Pretrained Embedding layer"""
@@ -58,5 +58,5 @@ class PretrainedEmbedding(object):
         """call pretrained embedding layer"""
         with tf.variable_scope(self.scope, reuse=tf.AUTO_REUSE):
             output_embedding = tf.nn.embedding_lookup(self.embedding, input_data)
-            
-            return output_embedding, self.embedding_placeholder
+        
+        return output_embedding, self.embedding_placeholder

reading_comprehension/layer/pooling.py
@@ -17,11 +17,11 @@ class MaxPooling(object):
                  input_mask):
         """call max pooling layer"""
         with tf.variable_scope(self.scope, reuse=tf.AUTO_REUSE):
-            output_mask = tf.squeeze(tf.reduce_max(input_mask, axis=-1, keep_dims=True), axis=-1)
+            output_mask = tf.reduce_max(input_mask, axis=-1, keep_dims=True)
             input_mask = tf.expand_dims(input_mask, axis=-1)
             output_pool = tf.reduce_max(input_data * input_mask, axis=-2)
-            
-            return output_pool, output_mask
+        
+        return output_pool, output_mask
 
 class AveragePooling(object):
     """average pooling layer"""
@@ -35,10 +35,10 @@ class AveragePooling(object):
                  input_mask):
         """call average pooling layer"""
         with tf.variable_scope(self.scope, reuse=tf.AUTO_REUSE):
-            output_mask = tf.squeeze(tf.reduce_max(input_mask, axis=-1, keep_dims=True), axis=-1)
+            output_mask = tf.reduce_max(input_mask, axis=-1, keep_dims=True)
             input_mask = tf.expand_dims(input_mask, axis=-1)
             input_sum = tf.reduce_sum(input_data * input_mask, axis=-2)
             input_count = tf.count_nonzero(input_mask, axis=-2)
             output_pool = 1.0 * input_sum / input_count
-            
-            return output_pool, output_mask
+        
+        return output_pool, output_mask

reading_comprehension/layer/recurrent.py
@@ -106,11 +106,11 @@ class RNN(object):
                  input_mask):
         """call uni-directional recurrent layer"""
         with tf.variable_scope(self.scope, reuse=tf.AUTO_REUSE):
-            input_length = tf.cast(tf.reduce_sum(input_mask, axis=-1), dtype=tf.int32)
+            input_length = tf.cast(tf.reduce_sum(tf.squeeze(input_mask, axis=-1), axis=-1), dtype=tf.int32)
             output_recurrent, final_state_recurrent = tf.nn.dynamic_rnn(cell=self.cell,
                 inputs=input_data, sequence_length=input_length, dtype=input_data.dtype)
-            
-            return output_recurrent, final_state_recurrent
+        
+        return output_recurrent, final_state_recurrent
 
 class BiRNN(object):
     """bi-directional recurrent layer"""
@@ -152,7 +152,7 @@ class BiRNN(object):
                  input_mask):
         """call bi-directional recurrent layer"""
         with tf.variable_scope(self.scope, reuse=tf.AUTO_REUSE):
-            input_length = tf.cast(tf.reduce_sum(input_mask, axis=-1), dtype=tf.int32)
+            input_length = tf.cast(tf.reduce_sum(tf.squeeze(input_mask, axis=-1), axis=-1), dtype=tf.int32)
             output_recurrent, final_state_recurrent = tf.nn.bidirectional_dynamic_rnn(cell_fw=self.fwd_cell,
                 cell_bw=self.bwd_cell, inputs=input_data, sequence_length=input_length, dtype=input_data.dtype)
             
@@ -163,5 +163,5 @@ class BiRNN(object):
                     state_list.append(final_state_recurrent[0][i])
                     state_list.append(final_state_recurrent[1][i])
                 final_state_recurrent = tuple(state_list)
-            
-            return output_recurrent, final_state_recurrent
+        
+        return output_recurrent, final_state_recurrent

reading_comprehension/util/data_util.py
@@ -179,13 +179,13 @@ def create_src_dataset(input_file,
     if subword_feat_enable == True:
         subword_pad_id = subword_vocab_index.lookup(tf.constant(subword_pad))
         subword_dataset = dataset.map(lambda sent: generate_subword_feat(sent, subword_vocab_index,
-            word_max_length, subword_max_length, subword_size, word_sos, word_eos, word_pad, subword_pad_id))
+            word_max_length, subword_max_length, subword_size, word_sos, word_eos, subword_pad, subword_pad_id))
 
     char_dataset = None
     if char_feat_enable == True:
         char_pad_id = char_vocab_index.lookup(tf.constant(char_pad))
         char_dataset = dataset.map(lambda sent: generate_char_feat(sent, char_vocab_index,
-            word_max_length, char_max_length, word_sos, word_eos, word_pad, char_pad_id))
+            word_max_length, char_max_length, word_sos, word_eos, char_pad, char_pad_id))
     
     return word_dataset, subword_dataset, char_dataset
 
@@ -232,7 +232,7 @@ def generate_subword_feat(sentence,
                           subword_size,
                           word_sos,
                           word_eos,
-                          word_pad,
+                          subword_pad,
                           subword_pad_id):
     """generate char feature for sentence"""
     def word_to_subword(word):
@@ -255,7 +255,7 @@ def generate_subword_feat(sentence,
     """process words for sentence"""
     words = tf.string_split([sentence], delimiter=' ').values
     words = tf.concat([[word_sos], words[:word_max_length], [word_eos],
-        tf.constant(word_pad, shape=[word_max_length])], axis=0)
+        tf.constant(subword_pad, shape=[word_max_length])], axis=0)
     max_length = word_max_length + 2
     words = words[:max_length]
     words = tf.dynamic_partition(words, tf.range(max_length), max_length)
@@ -271,7 +271,7 @@ def generate_char_feat(sentence,
                        char_max_length,
                        word_sos,
                        word_eos,
-                       word_pad,
+                       char_pad,
                        char_pad_id):
     """generate char feature for sentence"""
     def word_to_char(word):
@@ -288,7 +288,7 @@ def generate_char_feat(sentence,
     """process words for sentence"""
     words = tf.string_split([sentence], delimiter=' ').values
     words = tf.concat([[word_sos], words[:word_max_length], [word_eos],
-        tf.constant(word_pad, shape=[word_max_length])], axis=0)
+        tf.constant(char_pad, shape=[word_max_length])], axis=0)
     max_length = word_max_length + 2
     words = words[:max_length]
     words = tf.dynamic_partition(words, tf.range(max_length), max_length)