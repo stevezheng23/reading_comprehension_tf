191876270c691784fec0c693d2315c93c7e6350a
Mingzhi Zheng <mizheng@microsoft.com>
2018-08-10 14:12:17 -0700

    update dependency control fot exponential moving average

--- reading_comprehension/model/bidaf.py
+++ reading_comprehension/model/bidaf.py
@@ -119,7 +119,8 @@ class BiDAF(BaseModel):
                     with tf.control_dependencies([self.update_model]):
                         self.update_op = self.ema.apply(self.variable_list)
                     
-                    self.variable_lookup = {self.ema.average_name(v): self.ema.average(v) for v in self.variable_list}
+                    with tf.control_dependencies([self.update_op]):
+                        self.variable_lookup = {self.ema.average_name(v): self.ema.average(v) for v in self.variable_list}
                 else:
                     self.update_op = self.update_model
                 
