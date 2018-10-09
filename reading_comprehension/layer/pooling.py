fix seq max pooling masking issue

reading_comprehension/layer/pooling.py
@@ -21,8 +21,8 @@ class MaxPooling(object):
                  input_mask):
         """call max pooling layer"""
         with tf.variable_scope(self.scope, reuse=tf.AUTO_REUSE), tf.device(self.device_spec):
-            output_pool = tf.reduce_max(input_data * input_mask, axis=-2)
             output_mask = tf.squeeze(tf.reduce_max(input_mask, axis=-2, keepdims=True), axis=-2)
+            output_pool = tf.reduce_max(input_data * input_mask + MIN_FLOAT * (1 - input_mask), axis=-2) * output_mask
         
         return output_pool, output_mask
 